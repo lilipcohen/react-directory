906b8868a251006f9051a8602e2b91c8c4b84b30
Lili Cohen <lili.cohen.tv@gmail.com>
2021-03-14 14:42:34 -0700

    adding header for table

---
+++ employee-directory/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <tr>
+      <th>Name</th>
+      <th>Occupation</th>
+      <th>Zodiac Sign</th>
+      <th>Favorite Food</th>
+    </tr>
+  );
+}
+
+export default Header;
